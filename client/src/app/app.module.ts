0e09a4f546ea531573a7fbe0671636aeb06b571a
SQ <ritapyrh07@gmail.com>
2017-08-31 19:53:23 +0300

    Navbar path hotfix

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './src/app/components/navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
