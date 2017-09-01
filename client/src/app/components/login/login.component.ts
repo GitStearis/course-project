8174cb8b01c843fc9a6e0671b043640a2c114afd
GitStearis <stearis@tut.by>
2017-09-01 21:33:47 +0300

    Client login functionality

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  aveEmail(event: any) {
+    this.email = event.target.value;
+  }
+
+  saveUsername(event: any) {
+    this.username = event.target.value;
+  }
+
+  savePassword(event: any) {
+    this.password = event.target.value;
+  }
+
+  submit() {
+    console.log(this.email);
+    console.log(this.username);
+    console.log(this.password);
+
+    this.http.get("http://localhost:3000/login/" + this.email + "/" + this.username + "/" + this.password)
+      .subscribe(data => {
+        console.log(data);
+      }, err => {
+        if (err.error instanceof Error) {
+          console.log('An error occurred:', err.error.message);
+        }
+        else {
+          console.log(err);
+          console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        }
+      });
+  }
 }
