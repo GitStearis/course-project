0ed319cc0e2a73ae141ac55cb4aa4697aa8301ff
GitStearis <stearis@tut.by>
2017-09-01 20:46:32 +0300

    Added client registration functionality

--- client/src/app/components/registration/registration.component.ts
+++ client/src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-registration',
@@ -6,8 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  saveUsername(event: any) {
+    this.username = event.target.value;
+  }
+
+  savePassword(event: any) {
+    this.password = event.target.value;
+  }
+
+  submit() {
+    console.log(this.username);
+    console.log(this.password);
+
+    this.http.get("http://localhost:3000/registration/" + this.username + "/" + this.password)
+      .subscribe(data => {
+        console.log(data);
+      }, err => {
+        if (err.error instanceof Error) {
+          console.log('An error occurred:', err.error.message);
+        }
+        else {
+          console.log(err);
+          console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        }
+      });
+  }
 
   ngOnInit() {
   }
