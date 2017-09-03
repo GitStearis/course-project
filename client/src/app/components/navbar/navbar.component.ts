15ebb948ed2d74dfb57211020e2ada61ca9ae58c
SQ <ritapyrh07@gmail.com>
2017-09-03 10:41:02 +0300

    Added multistyling

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { RegistrationComponent } from '../registration/registration.component';
-import { LoginComponent } from '../login/login.component';
+import { Component, OnInit } from "@angular/core";
+import { RegistrationComponent } from "../registration/registration.component";
+import { LoginComponent } from "../login/login.component";
 
 import { AuthService } from "../../auth/auth.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  constructor(public auth: AuthService) {}
 
-  constructor(public auth: AuthService) { }
+  private style: string;
 
   ngOnInit() {
+    this.changeStyle("light");
   }
 
+  onChange() {
+    if (this.style === "light") this.changeStyle("dark");
+    else this.changeStyle("light");
+  }
+
+  changeStyle(style) {
+    this.style = style;
+    let links = document.getElementsByTagName("link");
+    for (let i = 0; i < links.length; i++) {
+      let link = links[i];
+      if (
+        link.getAttribute("rel").indexOf("style") != -1 &&
+        link.getAttribute("title")
+      ) {
+        link.disabled = true;
+        if (link.getAttribute("title") === this.style) link.removeAttribute('disabled');
+      }
+    }
+  }
 }
