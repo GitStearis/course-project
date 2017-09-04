adf265aadf5d9094406e1520cd7606e2e0047fca
George Puisha <stearis@tut.by>
2017-09-04 10:19:20 +0300

    Login hotfix

--- server/app/config/passport.js
+++ server/app/config/passport.js
@@ -6,27 +6,26 @@ const User = require("../models/users");
 mongoose.Promise = Promise;
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "email"
-    },
-    function(username, password, done) {
-      User.findOne({ email: username }, function(err, user) {
-        if (err) {
-          return done(err);
+    new LocalStrategy({
+            usernameField: "email"
+        },
+        function(username, password, done) {
+            User.findOne({ email: username }, function(err, user) {
+                if (err) {
+                    return done(err);
+                }
+                if (!user) {
+                    return done(null, false, {
+                        message: "User not found"
+                    });
+                }
+                if (!user.validPassword(password)) {
+                    return done(null, false, {
+                        message: "Password is wrong"
+                    });
+                }
+                return done(null, user);
+            });
         }
-        if (!user) {
-          return done(null, false, {
-            message: "User not found"
-          });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, {
-            message: "Password is wrong"
-          });
-        }
-        return done(null, user);
-      });
-    }
-  )
+    )
 );
