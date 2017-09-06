520873076e065690766b10bf6a5210f2c7a8f3f4
SQ <ritapyrh07@gmail.com>
2017-09-06 12:10:07 +0300

    Add simple markdown converter

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Http } from "@angular/http";
 
 import { routing, appRoutingProviders } from "./app.routing";
 
+import { MarkdownModule } from "angular2-markdown";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HomeComponent } from "./components/home/home.component";
@@ -16,11 +17,11 @@ import { NotfoundComponent } from "./components/notfound/notfound.component";
 import { DonationComponent } from "./components/donation/donation.component";
 import { LoginComponent } from "./components/login/login.component";
 import { ProjectComponent } from "./components/project/project/project.component";
-import { AdminComponent } from './components/admin/admin.component';
-import { PreviewComponent } from './components/project/preview/preview.component';
+import { AdminComponent } from "./components/admin/admin.component";
+import { PreviewComponent } from "./components/project/preview/preview.component";
 
 import { AuthService } from "./services/auth/auth.service";
-import { FormComponent } from './components/project/form/form.component';
+import { FormComponent } from "./components/project/form/form.component";
 
 @NgModule({
   declarations: [
@@ -37,7 +38,13 @@ import { FormComponent } from './components/project/form/form.component';
     PreviewComponent,
     FormComponent
   ],
-  imports: [HttpModule, HttpClientModule, BrowserModule, routing],
+  imports: [
+    HttpModule,
+    HttpClientModule,
+    BrowserModule,
+    routing,
+    MarkdownModule.forRoot()
+  ],
   providers: [appRoutingProviders, AuthService],
   bootstrap: [AppComponent]
 })
