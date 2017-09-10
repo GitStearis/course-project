ad9a908b3a9ec5984526a23791a07c18d109e938
SQ <ritapyrh07@gmail.com>
2017-09-10 22:20:15 +0300

    Try to make autocomplete tag input

--- client/src/app/components/project/create/create.component.ts
+++ client/src/app/components/project/create/create.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Profile } from "../../../profile";
-
 import { AuthService } from "../../../services/auth/auth.service";
 
 @Component({
   selector: "app-create",
+  host: {
+    "(document:click)": "handleClick($event)",
+  },
   templateUrl: "./create.component.html",
   styleUrls: ["./create.component.css"]
 })
@@ -21,7 +23,58 @@ export class CreateComponent implements OnInit {
   public deadline: string = "";
   public tags: string = "";
 
-  constructor(private http: HttpClient, public auth: AuthService) {}
+  public tagList: string[] = [];
+
+  public query = '';
+  public countries = [ "Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus",
+                      "Belgium","Bosnia & Herzegovina","Bulgaria","Croatia","Cyprus",
+                      "Czech Republic","Denmark","Estonia","Finland","France","Georgia",
+                      "Germany","Greece","Hungary","Iceland","Ireland","Italy","Kosovo",
+                      "Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta",
+                      "Moldova","Monaco","Montenegro","Netherlands","Norway","Poland",
+                      "Portugal","Romania","Russia","San Marino","Serbia","Slovakia","Slovenia",
+                      "Spain","Sweden","Switzerland","Turkey","Ukraine","United Kingdom","Vatican City"];
+  public filteredList = [];
+  public elementRef;
+
+  constructor(
+    private http: HttpClient,
+    public auth: AuthService,
+    public element: ElementRef
+  ) {
+    this.elementRef = element;
+  }
+
+  filter() {
+    if (this.query !== "") {
+      this.filteredList = this.countries.filter(
+        function(el) {
+          return el.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
+        }.bind(this)
+      );
+    } else {
+      this.filteredList = [];
+    }
+  }
+
+  select(item) {
+    this.query = item;
+    this.filteredList = [];
+  }
+
+  handleClick(event){
+    var clickedComponent = event.target;
+    var inside = false;
+    do {
+        if (clickedComponent === this.elementRef.nativeElement) {
+            inside = true;
+        }
+       clickedComponent = clickedComponent.parentNode;
+    } while (clickedComponent);
+     if(!inside){
+         this.filteredList = [];
+     }
+  }
 
   private getFromInput(value, target) {
     console.log(value);
@@ -97,6 +150,10 @@ export class CreateComponent implements OnInit {
           this.deafultErrorMessage(err);
         }
       );
+  }
+
+  public getTagList() {
+    this.tagList = ["one", "two"];
   }
 
   public submit() {
