a0bbf7da27b0abe270cb835d23ce840b7bcc27a7
SQ <ritapyrh07@gmail.com>
2017-09-20 19:16:55 +0300

    Add news' mail sending after news creation

--- server/app/config/mail.js
+++ server/app/config/mail.js
@@ -1,8 +1,6 @@
 const crypto = require("crypto");
 
 module.exports.transport = {
-  //host: "smtp.ethereal.email",
-  //port: 587,
   service: "gmail",
   port: 587,
   secure: false, // true for 465, false for other ports
@@ -20,7 +18,6 @@ module.exports.verificationToken = () => {
 }
 
 module.exports.verificationMail = (email, verificationToken, req) => {
-  // let link = "hhtp://" + req.get("host") + "/verify?id=" + rand;
   return config = {
     from: '"Margarita, George" <itra.courseproject@gmail.com>',
     to: email,
@@ -33,22 +30,29 @@ module.exports.verificationMail = (email, verificationToken, req) => {
   };
 };
 
-module.exports.successRegistration = email => {
+module.exports.newsMail = (email, subject, msg) => {
   let config = {
-    from: '"Margarita, George" <itra.courseproject@gmail.com>', // sender address
+    from: '"CO-OP Team" <itra.courseproject@gmail.com>', // sender address
     to: email,
-    subject: "Succesful registration ✔",
+    subject: subject,
     text: "Hello! Welcome to our awesome web-app!",
-    html:
-      "Hello! Welcome to our awesome web-app!"
+    html: msg
   };
   return config;
 }
 
 module.exports.sendMail = (err, info) => {
   if (err) {
-    console.log("Error occurred. " + err.message);
+    console.log(`Error occurred. ${err.message}`);
     return process.exit(1);
   }
-  console.log("Message sent: %s", info.messageId);
+  console.log(`Message sent: ${info.messageId}`);
 };
+
+module.exports.newsMailToFollowers = (news, followers) => {
+  let subject = `Check news on CO-OP ✔`;
+  let message = 
+    `Hello! You're following <a href='http://localhost:4200/project/${news.pageId}'>project</a> on CO-OP. 
+    Check the latest new via this <a href='http://localhost:4200/news/${news.newsId}'>link</a>`;
+  return newsMail(followers, subject, message);
+}
--- server/app/controllers/news-form.js
+++ server/app/controllers/news-form.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
 const News = require("../models/news");
+const Project = require("../models/projects");
+const nodemailer = require("nodemailer");
+const mailConfig = require("../config/mail");
 
 function hash(str) {
     return str.split('').reduce((prevHash, currVal) =>
@@ -25,9 +28,22 @@ module.exports.createNews = function(req, res) {
                 if (error) {
                     res.status(409).json("please, fill in all fields");
                 } else {
+                    let followersList = [];
+                    Project.findOne({pageId: _news.pageId}, (err, project) => {
+                        project.followers.map(follower => {
+                            followersList.push(follower.email);
+                        });
+                        sendNewsMail(_news, followersList);
+                    });
                     res.status(200).json(_news);
                 }
             });
         }
     });
+}
+
+function sendNewsMail(news, emails) {
+    let transporter = nodemailer.createTransport(mailConfig.transport);
+    let mailOptions = mailConfig.newsMailToFollowers(news, emails);
+    transporter.sendMail(mailOptions, mailConfig.sendMail);
 }
